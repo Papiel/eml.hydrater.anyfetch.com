dbfa555d207b70daf21bb8a234679582883842da
Hugo Duroux <hugoduroux@free.fr>
2014-01-08 16:31:45 +0100

    removing html markups when transfered to text

--- test/test.js
+++ test/test.js
@@ -32,4 +32,29 @@ describe('Test EML results', function() {
       done();
     });
   });
+  it('returns the correct informations', function(done) {
+    var document = {
+      datas: {},
+      metadatas: {},
+    };
+    eml(__dirname + "/samples/1.eml", document, function(err, document) {
+      if(err) {
+        throw err;
+      }
+
+      document.should.have.property('document_type', "email");
+      document.should.have.property('metadatas');
+      document.metadatas.should.have.property('to');
+      document.metadatas.should.have.property('cc');
+      document.metadatas.should.have.property('bcc');
+      document.metadatas.should.have.property('from');
+      document.metadatas.should.have.property('subject');
+      document.metadatas.should.have.property('text');
+      document.should.have.property('datas');
+      document.datas.should.have.property('html');
+
+      document.metadatas.text.should.include('Bonjour Ca va ?');
+      done();
+    });
+  });
 });
