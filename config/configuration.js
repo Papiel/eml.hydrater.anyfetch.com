3a6c189e9c4836cc0360cd68aecc8aa593e9750a
Amoki <hugoduroux@gmail.com>
2014-06-18 13:47:46 +0200

    more sockets

--- config/configuration.js
+++ config/configuration.js
@@ -17,6 +17,7 @@ if(node_env === "production") {
 module.exports = {
   env: node_env,
   port: process.env.PORT || default_port,
+  maxSockets: process.env.MAX_SOCKET || 50,
 
   concurrency: process.env.HYDRATER_EML_CONCURRENCY || default_concurrency
 };
