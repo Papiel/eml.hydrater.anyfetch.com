6713484aa7328cc510775ac04983a645ba3f125e
Matthieu Bacconnier <neamar@neamar.fr>
2014-06-25 18:30:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var config = require('./config/configuration.js');
 var hydrater = require('./lib/');
 var http = require('http');
 
-// See max socket value, default is 5.
+// Set max socket value, default is 5.
 // We need to do that as early as possible, before creating any server or connecting to mongoose
 // See http://markdawson.tumblr.com/post/17525116003/node
 http.globalAgent.maxSockets = config.maxSockets;
